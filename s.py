ed207467926034c6bc5c116befd2cb863ea18cd8
arsh23 <programmer.arsh@gmail.com>
2017-01-10 18:26:34 +0530

    Finished parse_text()

--- s.py
+++ s.py
@@ -1,52 +1,70 @@
 
+
 import time
-import requests
+# import requests
 from lxml import html
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 
 url = 'https://twitter.com/DelhiPolice'
+
 
 def get_tweet_objs(driver):
     tree = html.fromstring(driver.page_source)
     return tree.xpath('//li[@data-item-type="tweet"]')
 
-def extract_tweets(driver,user,num):
+
+def parse_text(tweet):
+    text = list()
+    for elem in tweet.xpath('.//p[contains(@class, "tweet-text")]/node()'):
+        if isinstance(elem, html.HtmlElement):
+            if elem.tag == 'a':
+                text += [' ', ''.join(elem.xpath('.//text()')), ' ']
+        else:
+            text += [' ', elem, ' ']
+    text = ''.join(text)
+    text = ' '.join(text.split())
+    text = text.encode('ascii', 'replace')
+    return text
+
+
+def parse_tweets(tweets):
+    for t in tweets:
+        # tweet = dict()
+        print(parse_text(t))
+
+
+def extract_tweets(driver, user, num):
     url = 'https://twitter.com/'+user
     driver.get(url)
 
+    print('Starting scraping...')
     tweets = list()
     while True:
         tweets_count = 0
-        for tweet in get_tweet_objs(driver): 
+        for tweet in get_tweet_objs(driver):
             if len(tweet.xpath('.//div[@class="context"]//text()')) == 1:
                 tweets_count += 1
         if tweets_count >= num:
-            print 'Got {} tweets for user : {}'.format(tweets_count,user)
+            print('Loaded {} tweets for user : {}'.format(tweets_count, user))
             break
         driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
         time.sleep(3)
 
-    for tweet in get_tweet_objs(driver): 
+    for tweet in get_tweet_objs(driver):
         if len(tweet.xpath('.//div[@class="context"]//text()')) == 1:
             tweets.append(tweet.xpath('.//div[@class="content"]'))
         if len(tweets) == num:
-            print 'Extracting {} tweets'.format(num)
+            print('Extracted {} tweets'.format(num))
             break
 
-    return tweets
-    
-
-
-
-
-
-
+    tweets = [x[0] for x in tweets]
+    return parse_tweets(tweets)
 
 
 if __name__ == '__main__':
     user = 'DelhiPolice'
     driver = webdriver.Firefox()
-    print extract_tweets(driver,user,5)
-
+    print(extract_tweets(driver,  user, 5))
+    driver.close()
