b603cefccc5b9b8158617c1f2fad6ea01740fc14
arsh23 <programmer.arsh@gmail.com>
2017-01-16 00:23:47 +0530

    updated analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 
+import re
 import pprint
 from collections import Counter
 from datetime import datetime as dt
@@ -15,22 +16,44 @@ usrs = 'DelhiPolice MumbaiPolice KolkataPolice punecitypolice hydcitypolice'
 def calc_top10(user):
     tweets = db[user].find({'hashtags': {'$gt': []}})
     counter = Counter(tag for tweet in tweets for tag in tweet['hashtags'])
-    return {x: y for x, y in counter.most_common(10)}
+    return [{'tag': x, 'amt': counter[x]} for x in counter.keys()]
 
 
 def get_top10(users):
     return {user: calc_top10(user) for user in users.split()}
 
 
+def get_dict(x, counter):
+    return {'day': x.split('-')[0], 'hr': x.split('-')[1], 'amt': counter[x]}
+
+
 def calc_freq(u):
     d = (dt.strptime(x['time'], '%I:%M %p - %d %b %Y') for x in db[u].find())
     counter = Counter('{}-{}'.format(x.weekday(), x.hour) for x in d)
-    return {x: counter[x] for x in counter.keys()}
+    return [get_dict(x, counter) for x in counter.keys()]
 
 
 def get_freq(users):
     return {user: calc_freq(user) for user in users.split()}
 
 
+'''
+for user in usrs.split():
+    tweets = db[user].find({'contains_imgs':True})
+    for tweet in tweets:
+        text = re.sub(r"pic.twitter.com\/[\w\d]+(?:\s|$)", '', tweet['text'])
+        if len(text.split()) <=3:
+            # print user,text, tweet['contains_imgs']
+            pass
+
+def calc_egmt(user):
+    i = db[user].find({'contains_imgs':True})
+    # regex =
+    t = (re.sub(r"pic.twitter.com\/[\w\d]+(?:\s|$)", '', x['text']) for x in i)
+    print len(t)
+
+for user in usrs.split():
+    calc_egmt(user)
+'''
 pprint.pprint(get_top10(usrs))
 pprint.pprint(get_freq(usrs))
